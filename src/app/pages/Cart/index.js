9e1f07ad0b8f06b4e32dba74b028f18da8931753
Martaalex <marta.aleksandrovic@gmail.com>
2019-07-30 18:34:56 +0300

    react eshop

--- src/app/pages/Cart/index.js
+++ src/app/pages/Cart/index.js
@@ -1,50 +1,59 @@
 import React from 'react';
 import './index.scss';
 
-function Cart({ products, cart }) {
-  const cartItems = cart.map(item => {
-    const product = products.find(({ id }) => id === item.id);
-
-    return { ...product, ...item };
-  });
-  const total = cartItems.reduce((result, { price, count }) => result + Number(price) * count, 0);
-
+function Error() {
   return (
-    <div className="Cart">
-      {!cart.length && (
-        <p>
-          Ohhh, no! You don't have anything in your cart{' '}
-          <span role="img" aria-label="crying face emoji">
-            😢
-          </span>
-        </p>
-      )}
-      {!!cartItems.length && (
-        <div className="Cart--header">
-          <label>Product:</label>
-          <label>Price:</label>
-        </div>
-      )}
-      {cartItems.map(({ id, name, price, currencySymbol, count }) => {
-        return (
-          <div key={id} className="Cart--item">
-            <span>
-              {name} x {count}
-            </span>
-            <span>
-              {price * count}
-              {currencySymbol}
-            </span>
-          </div>
-        );
-      })}
-      {!!cartItems.length && (
-        <div className="Cart--total">
-          <label>Total:</label> {total}
-        </div>
-      )}
+    <p>
+      Ohhh, no! You don't have anything in your cart{' '}
+      <span role="img" aria-label="crying face emoji">
+        😢
+      </span>
+    </p>
+  );
+}
+function CartHeader() {
+  return (
+    <div className="Cart--header">
+      <label>Product:</label>
+      <label>Price:</label>
     </div>
   );
 }
-
+function Total({ total }) {
+  return (
+    <div className="Cart--total">
+      <label>Total:</label> {total}
+    </div>
+  );
+}
+function CartRow({ name, count, price, currencySymbol }) {
+  return (
+    <div className="Cart--item">
+      <span>
+        {name} x {count}
+      </span>
+      <span>
+        {price * count}
+        {currencySymbol}
+      </span>
+    </div>
+  );
+}
+function Cart({ products, cart }) {
+  const cartItems = cart.map(item => {
+    const product = products.find(({ id }) => id === item.id);
+    return { ...product, ...item };
+  });
+  const total = cartItems.reduce((result, { price, count }) => result + Number(price) * count, 0);
+  return (
+    <div className="Cart">
+      {!cart.length && <Error />}
+      {!!cartItems.length && <CartHeader />}
+      {cartItems.map(item => (
+        <CartRow {...item} key={item.id} />
+      ))}
+      {!!cartItems.length && <Total total={total} />}
+    </div>
+  );
+}
 export default Cart;
